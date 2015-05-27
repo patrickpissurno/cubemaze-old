ea9d290e7d302ed4e9417851054dfeae0f9c8c05
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2015-05-26 21:13:09 -0300

    Sistema de gravidade update
    Agora o sistema de gravidade suporta rotacionar e se posicionar
    corretamente no alvo. OBS: O Sistema de gravidade só suporta colisores
    do tipo BoxCollider.

--- Assets/Storage/Scripts/CustomGravity.cs
+++ Assets/Storage/Scripts/CustomGravity.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class CustomGravity : MonoBehaviour {
 
     private new Rigidbody rigidbody;
-    public float gravityAmount = 25f;
 
     public GameObject target;
     public GameObject Target
@@ -16,11 +15,8 @@ public class CustomGravity : MonoBehaviour {
         set
         {
             target = value;
-            boost = Vector3.left;
         }
     }
-    private bool landed = false;
-    private Vector3 boost = Vector3.zero;
 
 	void Start () {
         this.rigidbody = this.GetComponent<Rigidbody>();
@@ -37,39 +33,15 @@ public class CustomGravity : MonoBehaviour {
     {
         if (target != null)
         {
-            //if (!landed)
-            {
-                //Vector3 vec = Quaternion.LookRotation(target.transform.position - transform.position).eulerAngles;
-                //transform.rotation = Quaternion.Euler(fixR(vec.x - 90), transform.rotation.eulerAngles.y, vec.z);
-                //this.rigidbody.AddForce((target.transform.position - transform.position).normalized * gravityAmount);
-            }
             Vector3 target_face = target.transform.position + target.transform.TransformDirection(Vector3.back * transform.localScale.z)/2;
+            Vector3 newRot = target.transform.rotation.eulerAngles + new Vector3(-90, 0, 0);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(fixR(newRot.x),fixR(newRot.y),fixR(newRot.z)), 0.2f);
             transform.position = Vector3.Lerp(transform.position, target_face, 0.2f);
         }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-        landed = true;
-        boost = Vector3.zero;
-    }
-
-    void OnCollisionExit(Collision col)
-    {
-        landed = false;
     }
 
     float fixR(float f)
     {
         return f % 360;
-    }
-
-    float GetVelocitySum()
-    {
-        float a = rigidbody.velocity.x;
-        a += rigidbody.velocity.y;
-        a += rigidbody.velocity.z;
-        a /= 3;
-        return a;
     }
 }
